d695347b896bc2cc49f4a2effc815c69d874615a
porpattawan <158016341+porpattawan@users.noreply.github.com>
2024-05-22 00:58:08 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,195 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Doi Pha Mi",
+          "type": "nature",
+          "level": 4.5,
+          "marker-color": "#28d411",
+          "marker-size": "medium",
+          "marker-symbol": "circle"
+        },
+        "geometry": {
+          "coordinates": [
+            99.85019927116275,
+            20.399777123235495
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Singha Park",
+          "type": "nature",
+          "marker-color": "#28d411",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.4
+        },
+        "geometry": {
+          "coordinates": [
+            99.73303256675956,
+            19.86037314470701
+          ],
+          "type": "Point"
+        },
+        "id": 1
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chiang Rai Walking Street",
+          "type": "special",
+          "marker-color": "#ff0095",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.5
+        },
+        "geometry": {
+          "coordinates": [
+            99.83510100000109,
+            19.90884728055137
+          ],
+          "type": "Point"
+        },
+        "id": 2
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cherntawan International Meditation Center",
+          "type": "cultural",
+          "marker-color": "#ff9500",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            99.87616852887601,
+            19.763298949069338
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Doi Tung",
+          "type": "cultural",
+          "marker-color": "#ff9500",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            99.8104465362502,
+            20.2887598269664
+          ],
+          "type": "Point"
+        },
+        "id": 4
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chiang Rai Clock Tower",
+          "type": "special",
+          "marker-color": "#ff0095",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.4
+        },
+        "geometry": {
+          "coordinates": [
+            99.8309944902889,
+            19.90712000083424
+          ],
+          "type": "Point"
+        },
+        "id": 5
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu Chee Fah",
+          "type": "ืnature",
+          "marker-color": "#28d411",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            100.4424709458795,
+            19.84932774702476
+          ],
+          "type": "Point"
+        },
+        "id": 6
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Rong Khun",
+          "type": "cultural",
+          "marker-color": "#ff9500",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.6
+        },
+        "geometry": {
+          "coordinates": [
+            99.76344095376203,
+            19.824153812815638
+          ],
+          "type": "Point"
+        },
+        "id": 7
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Night BarZaar",
+          "type": "special",
+          "marker-color": "#ff0095",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.3
+        },
+        "geometry": {
+          "coordinates": [
+            99.83344975374695,
+            19.905019954283546
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phra Kaeo",
+          "type": "cultural",
+          "marker-color": "#ff9500",
+          "marker-size": "medium",
+          "marker-symbol": "circle",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            99.8280302234312,
+            19.911817725556958
+          ],
+          "type": "Point"
+        },
+        "id": 9
+      }
+    ]
+  }
